db9a1a1ff1f03c2f4a592e4367549c8eeec565c1
LaKieshaHall <109048388+LaKieshaHall@users.noreply.github.com>
2022-11-22 10:38:14 -0500

    Update SQL Intro.sql

--- SQL Intro.sql
+++ SQL Intro.sql
@@ -1,51 +1,51 @@
 -- find all products
-Select * from products;
+SELECT * FROM products;
 
 -- find all products that cost $1400
-Select * From products
-Where price = 1400.00;
+SELECT * FROM products
+Where price = '1400.00';
 
 -- find all products that cost $11.99 or $13.99
-Select * From products
-Where price = 11.99 OR price = 13.99;
+SELECT * FROM products
+WHERE price = ‘11.99’ OR price = ‘13.99’;
 
 -- find all products that do NOT cost 11.99 - using NOT
-select * from products
-Where NOT price = 11.99;
+SELECT * FROM products
+WHERE NOT price = ‘11.99’;
 
 -- find all products and sort them by price from greatest to least
-select * from products
-Order by price desc;
+SELECT * FROM products
+ORDER BY price DESC;
 
 -- find all employees who don't have a middle initial
-select * from employees
-where middleinitial IS NULL;
+SELECT * FROM employees
+WHERE middleinitial IS NULL;
 
 -- find distinct product prices
-select distinct price
-from products;
+SELECT DISTINCT price
+FROM products;
 
 -- find all employees whose first name starts with the letter ‘j’
-select * from employees
-where firstname LIKE 'j%';
+SELECT * FROM employees
+WHERE firstname LIKE 'j%';
 
 -- find all Macbooks
-select * from products
-where name Like 'Macbook';
+SELECT * FROM products
+WHERE name LIKE 'Macbook';
 
 -- find all products that are on sale
-select * from products
-where Onsale = 1;
+SELECT * FROM products
+WHERE Onsale = ‘1’;
 
 -- find the average price of all products
-select avg(price)
-from products;
+SELECT AVG(price)
+FROM products;
 
 -- find all Geek Squad employees who don't have a middle initial
-select * from employees
-where middleinitial IS NULL;
+SELECT * FROM employees
+WHERE middleinitial IS NULL;
 
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword 
-select * from products
-where stocklevel between 500 AND 1200
-order by price;
+SELECT * FROM products
+WHERE stocklevel BETWEEN 500 AND 1200
+ORDER BY price;
